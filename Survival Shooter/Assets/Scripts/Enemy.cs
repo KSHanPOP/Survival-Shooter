4a7e3f324dd0d89e42ed559453cb098a5525c155
kim young woo <oos1928@gmail.com>
2023-01-09 21:31:56 +0900

    몬스터 이동

--- Survival Shooter/Assets/Scripts/Enemy.cs
+++ Survival Shooter/Assets/Scripts/Enemy.cs
@@ -1,18 +1,135 @@
 using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
-public class Enemy : MonoBehaviour
+public class Enemy : LivingEntity
 {
+    public LayerMask playerLayerMask;
+
+    private LivingEntity player;
+    private NavMeshAgent navMeshAgent;
+
+    public ParticleSystem hitEffect;
+    public AudioClip deathSound;
+    public AudioClip hurtSound;
+
+    private Animator enemyAnimator;
+    private AudioSource enemyAudioSource;
+
+    public float speed = 15f;
+    public float damage = 20f;
+    public float timeBetAttack = 0.5f;
+    private float lastAttackTime;
+
+    public float searchRadius = 20f;
+
+    private bool isPlayerAlive
+    {
+        get
+        {
+            return player != null && !player.dead;
+        }
+    }
+
+    private void Awake()
+    {
+        navMeshAgent = GetComponent<NavMeshAgent>();
+        enemyAnimator = GetComponent<Animator>();
+        enemyAudioSource = GetComponent<AudioSource>();        
+    }
+    public void Setup()
+    {
+        navMeshAgent.speed = speed;
+    }
     // Start is called before the first frame update
     void Start()
     {
-        
+        StartCoroutine(UpdatePath());
     }
+    private IEnumerator UpdatePath()
+    {
+        while(!dead)
+        {
+            if(isPlayerAlive)
+            {
+                navMeshAgent.isStopped = false;
+                navMeshAgent.SetDestination
+                    (player.transform.position);
+            }
+            else
+            {
+                navMeshAgent.isStopped = true;
+                
+                Collider[] colliders =
+                    Physics.OverlapSphere(transform.position, searchRadius, playerLayerMask);
 
+                for (int i = 0; i < colliders.Length; i++)
+                {
+                    LivingEntity livingEntity = colliders[i].GetComponent<LivingEntity>();
+
+                    if(livingEntity != null && !livingEntity.dead)
+                    {
+                        player = livingEntity;
+                        break;
+                    }                    
+                }                
+            }
+
+            yield return new WaitForSeconds(0.25f);
+        }        
+    }
+    public override void OnDamage(float damage, Vector3 hitPoint, Vector3 hitNormal)
+    {
+        if (dead)
+            return;
+
+        hitEffect.transform.position = hitPoint;
+        hitEffect.transform.rotation = Quaternion.LookRotation(hitNormal);
+        hitEffect.Play();
+
+        enemyAudioSource.PlayOneShot(hurtSound);
+
+        base.OnDamage(damage, hitPoint, hitNormal);        
+    }
+    public override void Die()
+    {
+        if (dead)
+            return;
+
+        base.Die();
+
+        Collider[] colliders = GetComponents<Collider>();
+        for (int i = 0; i < colliders.Length; i++)
+        {
+            colliders[i].enabled = false;
+        }
+
+        navMeshAgent.isStopped = true;
+        navMeshAgent.enabled = false;
+
+        enemyAnimator.SetTrigger("Die");
+        enemyAudioSource.PlayOneShot(deathSound);
+    }
+    private void OnTriggerStay(Collider other)
+    {
+        if (dead || Time.time < lastAttackTime + timeBetAttack)
+            return;
+
+        LivingEntity target = other.GetComponent<LivingEntity>();
+        if (target != null && target == player)
+        {
+            lastAttackTime = Time.time;
+
+            Vector3 hitPoint = other.ClosestPoint(transform.position);
+            Vector3 hitNormal = transform.position - other.transform.position;
+
+            target.OnDamage(damage, hitPoint, hitNormal);
+        }
+
+    }
     // Update is called once per frame
     void Update()
     {
-        
+        enemyAnimator.SetBool("IsPlayerDead", !isPlayerAlive);
     }
 }
--- Survival Shooter/Assets/Scripts/LivingEntity.cs
+++ Survival Shooter/Assets/Scripts/LivingEntity.cs
@@ -1,18 +1,37 @@
 using System.Collections;
 using System.Collections.Generic;
+using System;
 using UnityEngine;
 
-public class LivingEntity : MonoBehaviour
+public class LivingEntity : MonoBehaviour, IDamageable
 {
-    // Start is called before the first frame update
-    void Start()
+    public float health_Max = 100f;
+    public float health { get; protected set; }
+    public bool dead { get; protected set; }
+    public event Action onDeath;
+
+    protected virtual void OnEnable()
     {
-        
+        dead = false;
+        health = health_Max;
     }
 
-    // Update is called once per frame
-    void Update()
+    public virtual void OnDamage(float damage, Vector3 hitPoint, Vector3 hitNormal)
     {
-        
+        health -= damage;
+
+        if(health <= 0 && !dead)
+        {
+            Die();
+        }
+    }
+
+    public virtual void Die()
+    {
+        if(onDeath != null)
+        {
+            onDeath();
+        }
+        dead = true;
     }
 }
