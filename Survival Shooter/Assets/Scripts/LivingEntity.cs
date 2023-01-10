2391d5085af7d646df108936f643c34355ef60d0
DESKTOP-UM7NU9E\kshan <kshan9279@gmail.com>
2023-01-10 10:49:29 +0900

    플레이어 체력

---
+++ Survival Shooter/Assets/Scripts/LivingEntity.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using System;
+using UnityEngine;
+
+public class LivingEntity : MonoBehaviour, IDamageable
+{
+    public float health_Max = 100f;
+    public float health { get; protected set; }
+    public bool dead { get; protected set; }
+    public event Action onDeath;
+
+    protected virtual void OnEnable()
+    {
+        dead = false;
+        health = health_Max;
+    }
+
+    public virtual void OnDamage(float damage, Vector3 hitPoint, Vector3 hitNormal)
+    {
+        health -= damage;
+
+        if (health <= 0 && !dead)
+        {
+            Die();
+        }
+    }
+
+    public virtual void Die()
+    {
+        if (onDeath != null)
+        {
+            onDeath();
+        }
+        dead = true;
+    }
+}
